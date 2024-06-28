f725b2dadda39167473ae9ee1c622c7dcb3543fa
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-06-28 22:42:51 +0300

    working?

--- frontend/src/services/imageService.ts
+++ frontend/src/services/imageService.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://nitrojamrec.
 
 export const fetchImageUrl = async (directory: string, imageName: string): Promise<string> => {
   try {
-    const url = `${API_BASE_URL}/api/images/${directory}/${imageName}`
+    const url = `${API_BASE_URL}/nj_api/images/${directory}/${imageName}`
     console.log(`Fetching image from ${url}`)
 
     const response = await axios.get(url, {
