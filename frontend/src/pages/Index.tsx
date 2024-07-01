c2459ba6958416c45cf5dfdc0f3dac751e2f500d
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-07-02 01:44:10 +0300

    remove lazy

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,30 +1,18 @@
-import React, { Suspense, useEffect } from 'react';
-
-const HeaderBanner = React.lazy(() => import('../components/HeaderBanner/HeaderBanner').then(module => ({ default: module.default })));
-const MusicBlock = React.lazy(() => import('../components/MusicBlock/MusicBlock').then(module => ({ default: module.default })));
-const Special = React.lazy(() => import('../components/Special/Special').then(module => ({ default: module.default })));
-const SliderBlock = React.lazy(() => import('../components/Slider/SliderBlock').then(module => ({ default: module.default })));
-
-const preloadComponent = (importFn: () => Promise<{ default: React.ComponentType<any> }>) => {
-  importFn().then(module => module.default);
-};
+import React from 'react'
+import { HeaderBanner } from '../components/HeaderBanner/HeaderBanner'
+import MusicBlock from '../components/MusicBlock/MusicBlock'
+import Special from '../components/Special/Special'
+import {SliderBlock} from '../components/Slider/SliderBlock'
 
 export const IndexPage: React.FC = () => {
-  useEffect(() => {
-    preloadComponent(() => import('../components/HeaderBanner/HeaderBanner').then(module => ({ default: module.default })));
-    preloadComponent(() => import('../components/MusicBlock/MusicBlock').then(module => ({ default: module.default })));
-    preloadComponent(() => import('../components/Special/Special').then(module => ({ default: module.default })));
-    preloadComponent(() => import('../components/Slider/SliderBlock').then(module => ({ default: module.default })));
-  }, []);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <>
       <HeaderBanner />
       <MusicBlock />
       <Special />
       <SliderBlock />
-    </Suspense>
-  );
+    </>
+  )
 }
 
-export default IndexPage;
+export default IndexPage
