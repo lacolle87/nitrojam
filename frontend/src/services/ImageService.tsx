2055fa23085f8926267a70508e184a755379ede3
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-07-02 13:01:54 +0300

    refactor ImageService.tsx

--- frontend/src/services/ImageService.tsx
+++ frontend/src/services/ImageService.tsx
@@ -1,29 +1,28 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || ''
 
-const imageUrlCache: Record<string, string> = {};
-
+const imageUrlCache: Record<string, string> = {}
 
 export const fetchImageUrl = async (
   directory: string,
   imageName: string
 ): Promise<string> => {
+  const url = `${API_BASE_URL}/nj_api/images/${directory}/${imageName}`;
+
+  if (imageUrlCache[url]) {
+    return imageUrlCache[url]
+  }
+
   try {
-    const url = `${API_BASE_URL}/nj_api/images/${directory}/${imageName}`
-
-    if (imageUrlCache[url]) {
-      return imageUrlCache[url]
-    }
-
-    const response: AxiosResponse<Blob> = await axios.get(url, {
+    const response = await axios.get<Blob>(url, {
       responseType: 'blob',
       headers: {
-        'Accept': 'image/*',
+        Accept: 'image/*',
       },
-    })
+    });
 
-    console.log("Response received", response)
+    console.log('Response received', response)
 
     imageUrlCache[url] = URL.createObjectURL(new Blob([response.data]))
     return imageUrlCache[url];
