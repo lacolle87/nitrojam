5746c66c685f1683252ef9706e45bb4a08738992
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-08-04 23:21:16 +0300

    lazy

--- react/src/pages/Index.tsx
+++ react/src/pages/Index.tsx
@@ -1,17 +1,20 @@
-import HeaderBanner from '../components/HeaderBanner/HeaderBanner'
-import MusicBlock from '../components/MusicBlock/MusicBlock'
-import Special from '../components/Special/Special'
-import FaqBlock from '../components/FaqBlock/FaqBlock'
+import React, { Suspense } from 'react';
+
+// Lazy load the components
+const HeaderBanner = React.lazy(() => import('../components/HeaderBanner/HeaderBanner'));
+const MusicBlock = React.lazy(() => import('../components/MusicBlock/MusicBlock'));
+const Special = React.lazy(() => import('../components/Special/Special'));
+const FaqBlock = React.lazy(() => import('../components/FaqBlock/FaqBlock'));
 
 function IndexPage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <HeaderBanner />
       <MusicBlock />
       <Special />
       <FaqBlock />
-    </>
-  )
+    </Suspense>
+  );
 }
 
-export default IndexPage
+export default IndexPage;
