5ad37a89f1328c2d98283e273e5cdcb1740cd719
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-08-04 23:26:57 +0300

    lazy remove

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,51 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import AppLayout from './AppLayout';
-import React from 'react'
-
-const IndexPage = React.lazy(() => import('./pages/Index'));
-const AboutPage = React.lazy(() => import('./pages/About'));
-const ServicesPage = React.lazy(() => import('./pages/Services'));
-const ContactPage = React.lazy(() => import('./pages/Contact'));
-const PrivacyPage = React.lazy(() => import('./pages/Privacy'));
-const PhotoPage = React.lazy(() => import('./pages/Photo'));
+import { Routes, Route } from 'react-router-dom'
+import AppLayout from './AppLayout'
+import IndexPage from './pages/Index'
+import AboutPage from './pages/About'
+import ServicesPage from './pages/Services'
+import ContactPage from './pages/Contact'
+import PrivacyPage from './pages/Privacy'
+import PhotoPage from './pages/Photo'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route index element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <IndexPage />
-          </React.Suspense>
-        } />
-        <Route path="about_us" element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <AboutPage />
-          </React.Suspense>
-        } />
-        <Route path="services" element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <ServicesPage />
-          </React.Suspense>
-        } />
-        <Route path="contact" element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <ContactPage />
-          </React.Suspense>
-        } />
-        <Route path="privacy" element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <PrivacyPage />
-          </React.Suspense>
-        } />
-        <Route path="photos" element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <PhotoPage />
-          </React.Suspense>
-        } />
+        <Route index element={<IndexPage />} />
+        <Route path="about_us" element={<AboutPage />} />
+        <Route path="services" element={<ServicesPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="privacy" element={<PrivacyPage />} />
+        <Route path="photos" element={<PhotoPage />} />
       </Route>
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
